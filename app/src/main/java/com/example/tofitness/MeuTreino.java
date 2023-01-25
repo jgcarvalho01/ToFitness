38bd591586ed81376292d2b583a648d396d96132
jgcarvalho01 <gabrielcarvalhooak@gmail.com>
2023-01-25 17:15:24 -0300

    configurando telas de meu treino e relatório

--- app/src/main/java/com/example/tofitness/MeuTreino.java
+++ app/src/main/java/com/example/tofitness/MeuTreino.java
@@ -2,13 +2,41 @@ package com.example.tofitness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class MeuTreino extends AppCompatActivity {
+
+    private ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meu_treino);
+
+        BancoController crud = new BancoController(getBaseContext());
+        final Cursor cursor = crud.listarTreinos();
+
+        String[] nomeCampos = new String[]{CriaBanco.CATEGORIA_TREINO, CriaBanco.TIPO_TREINO, CriaBanco.REPETICAO_TREINO};
+        int[] idViews = new int[]{R.id.categoria, R.id.tipo, R.id.repeticao};
+
+        SimpleCursorAdapter adaptador = new SimpleCursorAdapter(getBaseContext(), R.layout.treino_layout, cursor, nomeCampos, idViews, 0);
+        lista = findViewById(R.id.lvTreino);
+        lista.setAdapter(adaptador);
+
+    }
+
+    public void IrParaAddTreino(View v) {
+        Intent i = new Intent(MeuTreino.this, Treino_Form.class);
+        startActivity(i);
+    }
+
+    public void Voltar(View v) {
+        Intent i = new Intent(MeuTreino.this, MainActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/tofitness/Relatorio.java
+++ app/src/main/java/com/example/tofitness/Relatorio.java
@@ -2,13 +2,41 @@ package com.example.tofitness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class Relatorio extends AppCompatActivity {
+
+    private ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relatorio);
+
+        BancoController crud = new BancoController(getBaseContext());
+        final Cursor cursor = crud.listarRelatorio();
+
+        String[] nomeCampos = new String[]{CriaBanco.ALTURA_RELATORIO, CriaBanco.PESO_RELATORIO, CriaBanco.IMC_RELATORIO, CriaBanco.DATA_RELATORIO};
+        int[] idViews = new int[]{R.id.altura, R.id.peso, R.id.imc, R.id.data};
+
+        SimpleCursorAdapter adaptador = new SimpleCursorAdapter(getBaseContext(), R.layout.relatorio_layout, cursor, nomeCampos, idViews, 0);
+        lista = findViewById(R.id.lvRelatorio);
+        lista.setAdapter(adaptador);
+
+    }
+
+    public void IrParaAddRelatorio(View v) {
+        Intent i = new Intent(Relatorio.this, Relatorio_Form.class);
+        startActivity(i);
+    }
+
+    public void Voltar(View v) {
+        Intent i = new Intent(Relatorio.this, MainActivity.class);
+        startActivity(i);
     }
 }
