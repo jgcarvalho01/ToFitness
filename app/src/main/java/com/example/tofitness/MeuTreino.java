89425178efa174f5bab89fb9d0ba6831d8fdf9e4
jgcarvalho01 <gabrielcarvalhooak@gmail.com>
2023-01-25 01:53:01 -0300

    criando as telas "Meu Treino" e "Relatorio"

---
+++ app/src/main/java/com/example/tofitness/MeuTreino.java
@@ -0,0 +1,14 @@
+package com.example.tofitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MeuTreino extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meu_treino);
+    }
+}
